c64d5acbd446d58a379c954dc6c9cf4791bbf158
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-12 11:09:06 -0600

    Update exercises.js

--- models/exercises.js
+++ models/exercises.js
@@ -3,8 +3,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-    day: Date,
+    day: { type: Date, default: Date.now },
     exercises: Array
+}, {toJSON: {virtuals: true}});
+
+exerciseSchema.virtual("totalDuration").get(function () {
+    let totalDuration = this.exercises.reduce((total, exercise)=>{return total + exercise.duration}, 0);
+    return totalDuration; 
 });
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
